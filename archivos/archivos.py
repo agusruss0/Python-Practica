86e7fc39f7202c0703ca17cf49670515ad036bea
Agustin Russo <agus.drum12@gmail.com>
2023-11-29 19:48:04 -0300

    archivos

---
+++ archivos/archivos.py
@@ -0,0 +1,18 @@
+path = ""
+
+#forma de abrir archivos
+with open(path, 'rt', encoding='utf-8') as f:
+    #codigo
+    f.close()
+
+def cantidad_de_palabras(): #devuelve la cantidad de palabras del txt
+    return
+
+def tiene_palabra(): #cuenta la cantidad de apariciones de una palabra
+    return 
+
+def palabra_mas_larga(): #devuelve la palabra mas larga del texto
+    return
+
+def dicc_de_palabras(): #devuelve un diccionario de la forma(palabara:cantidad de apariciones)
+    return
--- basico/basico.py
+++ basico/basico.py
@@ -26,3 +26,22 @@ def sumar_unos():
 def mezclar():
     return
 
+#Extras
+def es_primo(): #devuelve true o false si el n dado es primo o no
+    return
+
+#FACTORIAL: 3!=1*2*3=6, 0!=1
+def factorial_ite(): #devuelve el factorial de un numero de forma iterada
+    return
+
+def factorial_recu(): #mismo que el anterior pero usando recursion
+    return
+
+def matriz_por_escalar(): #multiplica cada elemento de una matriz por n
+    return
+
+def suma_gauss(): #suma todos los nuemeros hasta el n dado
+    return
+
+def geringoso(): # recibe una palabra y la convierte en geringoso -> voto->vopotopo
+    return
