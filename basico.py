645f8e17bfcee0994868cef43d1d8d522e96a1a8
Agustin Russo <agus.drum12@gmail.com>
2023-11-29 15:48:10 -0300

    Create basico.py

---
+++ basico.py
@@ -0,0 +1,28 @@
+#1
+def es_par ():
+    return 
+    
+#2
+def dos_pertenece():
+    return
+    
+#3
+def pertenece():
+    return
+
+#4
+def mas_larga():
+    return
+ 
+#5
+def cant_e():
+    return 
+ 
+#6
+def sumar_unos():
+    return
+
+#7
+def mezclar():
+    return
+
